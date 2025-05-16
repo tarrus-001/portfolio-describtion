5f9fbe97d71f5c95b410ff003078201aa80f14d1
tarrus-001 <80381888+tarrus-001@users.noreply.github.com>
2025-05-16 08:55:25 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> TekTech </span>{" "}
+            <span className="text-glow text-foreground"> TeKTech </span>{" "}
             Portfolio
           </span>
         </a>
